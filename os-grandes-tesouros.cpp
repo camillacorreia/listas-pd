43f097b8c0d998d233973b651d5b26df75e28a53
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-08-26 12:20:18 -0300

    feat: exercise 1

---
+++ os-grandes-tesouros.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t1, t2, t3, t4, t5, t6, total;
+  cin >> t1 >> t2 >> t3 >> t4 >> t5 >> t6;
+  total = t1+t2+t3+t4+t5+t6;
+  cout << total << endl;
+}
